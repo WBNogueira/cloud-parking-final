b6a18d80ed59f007b0a55d2f291336c3ea729d43
WB Nogueira <wbnogueira3@gmail.com>
2022-08-17 16:05:11 -0300

    Criado arquivo system oriperties

---
+++ src/test/java/one/digitalinnovation/parking/controller/AbstractContainerBase.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.parking.controller;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class AbstractContainerBase {
+
+    static final PostgreSQLContainer POSTEGRE_SQL_CONTAINER;
+
+    static {
+        POSTEGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:10-alpine");
+        POSTEGRE_SQL_CONTAINER.start();
+        System.setProperty("spring.datasource.url", POSTEGRE_SQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.datasource.username", POSTEGRE_SQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", POSTEGRE_SQL_CONTAINER.getPassword());
+    }
+}
